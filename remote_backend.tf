b1c61580ddea5701665d94917a3168104b87b3ff
Pradeep <160380277+pkumard4@users.noreply.github.com>
2024-12-17 13:35:01 +0530

    Update remote_backend.tf

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "Training_on_Terraform_17122024"
+    bucket         = "training-bangalore-195"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
